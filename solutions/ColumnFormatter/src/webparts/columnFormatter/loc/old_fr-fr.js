d99fc19be24a8c82a21d1be47131c5b68b2020cd
Richard Gigan <rgigan@circum.fr>
2018-01-18 14:27:58 +0100

    French translation

---
+++ solutions/ColumnFormatter/src/webparts/columnFormatter/loc/old_fr-fr.js
@@ -0,0 +1,215 @@
+define([], function () {
+  return {
+    'PropertyPaneDescription': 'Description',
+    'BasicGroupName': 'Group Name',
+    'DescriptionFieldLabel': 'Description Field',
+
+    //Welcome
+    WelcomeTitle: 'Column Formatter',
+    WelcomeSubTitle: 'Éditeur simplifier pour la mise en forme de colonne pour l\'expérience moderne',
+    WelcomeNewHeader: 'Nouveau',
+    WelcomeNewDescription: 'Commencez à partir de zéro ou choisissez un modèle',
+    WelcomeOpenHeader: 'Ouvrir',
+    WelcomeOpenDescription: 'Charger depuis une bibliothèque ou récupéré d\'une liste locale',
+    WelcomeNewColumnTypeLabel: 'Type de colonne',
+    WelcomeNewWizardOption: 'Commencez avec un modèle',
+    WelcomeNewBlankOption: 'Commencer à partir de zéro',
+    WelcomeBackButton: 'Précedent',
+    WelcomeOKButton: 'OK',
+    WelcomeNextButton: 'Suivant',
+    WelcomeOpenLoadList: 'Charger à partir d\'une liste locale',
+    WelcomeOpenLoadFile: 'Charger à partir d\'un fichier :',
+    WelcomeOpenColumnTypeLabel: 'Type de colonne',
+    WelcomeOpenLoadFileLibrary: 'Ouvrir un fichier à partir d\'une bibliothèque locale',
+    WelcomeOpenLoadFileUpload: 'Téléverser un fichier',
+
+    WelcomeOpenLoadFileUploadTitle: 'Téléversement d\'un fichier',
+    WelcomeOpenLoadFileUploadDesc: 'Déposez votre fichier json ici, ou cliquez pour sélectionner le fichier à télécharger. Seuls les fichiers *.json seront acceptés',
+    WelcomeOpenLoadFileUploadChooseBtnLabel: 'Choisissez un fichier',
+    WelcomeOpenLoadFileUploadRejectError:'Impossible d\'accepter :',
+    WelcomeOpenLoadFileUploadFileError:'Le fichier est vide !',
+
+    //Tab Names
+    TabWizard: 'Assistant',
+    TabTree: 'Arbre',
+    TabData: 'Donnée',
+    TabPreview: 'Aperçu',
+    TabCode: 'Code',
+    TabSplit: 'Cote à cote',
+
+    //Panel Headers
+    PanelHeaderData: 'Valeurs de test',
+    PanelHeaderPreview: 'Aperçu',
+    PanelHeaderCode: 'Éditeur de code',
+
+    //Editor CommandBar
+    CommandNew: 'Nouveau',
+    CommandCustomize: 'Personnaliser',
+    CommandEditor: 'Thème',
+    CommandSaveAs: 'Enregistrer sous',
+    CommandDownload: 'Télécharger',
+    CommandCopy: 'Copier dans le presse-papier',
+    CommandSaveToLibrary: 'Copier dans le presse-papier',
+    CommandApplyToList: 'Appliquer au champ de liste local',
+
+    // Local list
+    LocalListDropdownTitle: 'Liste locale',
+    LocalListFieldTitle: 'Champ',
+
+    // Tree
+    TreePanelElementsTreeTitle: 'Elements de l\'arbre',
+    TreePanelElementsTreeError: 'Erreur lors du chargement de l\'arbre ! Détails techniques :',
+
+    //New Confirmation Dialog
+    NewConfirmationDialogTitle: 'Recommencer ?',
+    NewConfirmationDialogText: 'Tout changement non enregistré sera perdu. Voulez-vous continuer ?',
+    NewConfirmationDialogConfirmButton: 'Oui',
+    NewConfirmationDialogCancelButton: 'Annuler',
+
+    //Customize Confirmation Dialog
+    CustomizeConfirmationDialogTitle: 'Enlever l\'assistant ?',
+    CustomizeConfirmationDialogText: 'Vous serez en mesure de modifier le code directement, mais le volet de l\'assistant ne sera plus disponible. Ceci est pour les utilisateurs avancés. Êtes-vous sûr ?',
+    CustomizeConfirmationDialogConfirmButton: 'Oui',
+    CustomizeConfirmationDialogCancelButton: 'Annuler',
+
+    //Save To Library Dialog
+    SaveToLibraryDialogTitle: 'Enregistrer dans la bibliothèque locale',
+    SaveToLibraryDialogConfirmButton: 'Enregistrer',
+    SaveToLibraryDialogCancelButton: 'Annuler',
+
+    //Apply To List Dialog
+    ApplyToListDialogTitle: 'Appliquer au champ de liste local',
+    ApplyToListDialogConfirmButton: 'Enregistrer',
+    ApplyToListDialogCancelButton: 'Annuler',
+
+    //Data Column/Row buttons
+    DeleteRow: 'Supprimer la ligne',
+    AddRow: 'Ajouter une ligne    ',
+    DeleteColumn: 'Supprimer le champ',
+    AddColumn: 'Ajouter le champ',
+
+    //Data Column Editing
+    ColumnNameChangeTooltip: 'Nom du champ interne',
+    ColumnTypeHeadline: 'Type de colonne',
+    ColumnTypeMessage: 'Changer le type réinitialisera également les valeurs',
+    ColumnTypeChangeTooltip: 'Cliquez pour changer',
+    SubPropertiesHeadline: 'Sous Propriétés',
+    TimeHeadline: 'Temps',
+
+    //Column Type Names
+    ColumnTypeBoolean: 'Oui/Non',
+    ColumnTypeChoice: 'Choix',
+    ColumnTypeDateTime: 'Date / heure',
+    ColumnTypeLink: 'Hyperlien',
+    ColumnTypePicture: 'Image',
+    ColumnTypeLookup: 'Lookup',
+    ColumnTypeNumber: 'Nombre',
+    ColumnTypePerson: 'Personne',
+    ColumnTypeText: 'Texte',
+    ColumnTypeUnknown: 'Inconnu',
+
+    //Boolean Values
+    BoolValueStringTrue: 'Oui',
+    BoolValueStringFalse: 'Non',
+
+    //DateTime Editor Strings
+    Month1: 'Janvier',
+    Month2: 'Février',
+    Month3: 'Mars',
+    Month4: 'Avril',
+    Month5: 'Mai',
+    Month6: 'Juin',
+    Month7: 'Juillet',
+    Month8: 'Août',
+    Month9: 'Septembre',
+    Month10: 'Octobre',
+    Month11: 'Novembre',
+    Month12: 'Décembre',
+    Month1Short: 'Jan.',
+    Month2Short: 'Fev.',
+    Month3Short: 'Mar.',
+    Month4Short: 'Avr.',
+    Month5Short: 'Mai',
+    Month6Short: 'Juin',
+    Month7Short: 'Jui.',
+    Month8Short: 'Aou.',
+    Month9Short: 'Sep.',
+    Month10Short: 'Oct.',
+    Month11Short: 'Nov.',
+    Month12Short: 'Déc.',
+    Day1: 'Dimanche',
+    Day2: 'Lundi',
+    Day3: 'Mardi',
+    Day4: 'Mercredi',
+    Day5: 'Jeudi',
+    Day6: 'Vendredi',
+    Day7: 'Samedi',
+    Day1Short: 'D',
+    Day2Short: 'L',
+    Day3Short: 'M',
+    Day4Short: 'M',
+    Day5Short: 'J',
+    Day6Short: 'V',
+    Day7Short: 'S',
+    DTgoToToday: 'Aujourd\'hui',
+    DTprevMonthAriaLabel: 'Aller au mois précédent',
+    DTnextMonthAriaLabel: 'Aller au mois suivant',
+    DTprevYearAriaLabel: 'Aller à l\'année précédente',
+    DTnextYearAriaLabel: 'Aller à l\'année prochaine',
+    HourLabel: 'Heure',
+    MinuteLabel: 'Minute',
+    SecondsLabel: 'Seconde',
+
+    //Custom Formatting Error Strings
+    CFSariaError: 'Aucune étiquette trouvée. En tant que tel, le champ ne sera pas accessible via un lecteur d\'écran.',
+    CFSelmTypeInvalid: 'ElmType non valide: {0}. Doit être l\'un de {1}.',
+    CFSelmTypeMissing: 'Doit spécifier elmType.',
+    CFSinvalidProtocol: 'Une URL a été bloquée. Seuls les protocoles http, https et mailto sont autorisés.',
+    CFSinvalidStyleAttribute: '"{0}" n\'est pas un attribut de style pris en charge.',
+    CFSinvalidStyleValue: 'Les valeurs de style "{0}" contiennent un ou plusieurs des caractères non autorisés suivants ( : & ; ! .',
+    CFSnan: '{0} n\'est pas un nombre. Nombre attendu dans l\'expression {1}.',
+    CFSoperandMissing: 'Il doit y avoir au moins 1 opérande dans l\'expression {0}.',
+    CFSoperandNOnly: '{0} attendant opérande (s) pour l\'expression {1}.',
+    CFSoperatorInvalid: '"{0}" n\'est pas un opérateur valide. Ce doit être l\'un de {1} dans l\'expression {2}.',
+    CFSoperatorMissing: 'Opérateur manquant dans l\'expression: {0}.',
+    CFSunsupportedType: 'Le type de champ {0} n\'est pas pris en charge pour le moment.',
+    CFSuserFieldError: 'Le champ "{0}" est de type "Utilisateur" et ne peut pas être utilisé directement car il possède des sous-propriétés. Vous devez spécifier la sous-propriété que vous souhaitez utiliser. par exemple : [$ AssignedTo.email]',
+
+    //Format Validation Messages
+    PreviewValidationGood: 'Validation passée !',
+    PreviewValidationBad: 'JSON (code) incorrect :',
+
+    //Wizard Data Bars
+    WizardDataBarsEmptyBarLabel: 'Basse :',
+    WizardDataBarsEmptyBarTooltip: 'La valeur la plus basse sur l\'échelle \nLes valeurs égales ou inférieures à cela seront affichées à 0%',
+    WizardDataBarsFullBarLabel: 'Haute:',
+    WizardDataBarsFullBarTooltip: 'La valeur la plus élevée sur l\'échelle \nLes valeurs égales ou supérieures à celle-ci seront affichées comme étant à 100%',
+    WizardDataBarsRangeGroupLabel: 'Fourchette',
+    WizardDataBarsValueDisplayGroupLabel: 'Affichage de la valeur',
+    WizardDataBarsValueDisplayActual: 'Afficher la valeur réelle',
+    WizardDataBarsValueDisplayPercentage: 'Afficher le pourcentage',
+    WizardDataBarsValueDisplayNone: 'Aucun',
+
+    WizardInfoDataBarsTitle: 'Barres de données',
+    WizardInfoDataBarsDesc: 'Ajoute des barres horizontales au champ pour exprimer visuellement la valeur par longueur',
+
+    // Wizard Trending
+    WizardInfoTrendingTitle: '# Tendance',
+    WizardInfoTrendingDesc: 'Compare les autres champs de la ligne pour fournir une icône basée sur les valeurs de tendance',
+
+    // Wizard Overdue
+    WizardInfoOverdueTitle: 'En retard',
+    WizardInfoOverdueDesc: 'Couleurs le champ rouge une fois que la date est plus grande qu\'aujourd\'hui',
+
+    // Wizard standard
+    WizardStartingColumnsTitle: 'MonChamp',
+
+    // Wizard mail
+    WizardInfoMailTitle: 'Mail',
+    WizardInfoMailDesc: 'pout test, ce n\'est pas un vrai assistant',
+
+    // Wizard checkbox
+    WizardInfoCheckboxesTitle: 'Cases à cocher',
+    WizardInfoCheckboxesDesc: 'Affiche les champs Oui / Non en tant que cases à cocher'
+  }
+});
