990e96c69506b1e7efa352b130a691d72f8fdd3c
Yannick Reekmans <YannickRe@users.noreply.github.com>
2019-04-15 07:36:38 +0200

    [ModernSearch] Improve search navigation (#130)
    * Reworked the dynamic data selection to be in line with the other web parts, only allowing search box as a selection
    
    * Added ability to sort navigation nodes
    
    * Made sure that the homepage of a site (without page) also gets marked "selected" when full url (with page) exists in the navigation nodes

--- solutions/ModernSearch/react-search-refiners/spfx/src/webparts/searchNavigation/loc/en-us.js
+++ solutions/ModernSearch/react-search-refiners/spfx/src/webparts/searchNavigation/loc/en-us.js
@@ -1,13 +1,17 @@
 define([], function() {
   return {
-    DynamicFieldSetLabel: "Searchword source",
-    DynamicFieldLabel: "Searchword",
-    NavNodeLabel: "Navigation nodes",
-    NavNodeHeader: "Navigation nodes",
-    NavNodeManageBtnLabel: "Setup",
-    NavNodeDisplayTextFieldLabel: "Displaytext",
-    NavNodeUrlFieldLabel: "Url",
-    ColorPickerLabel: "Color",
-    UseThemeColorLabel: "User primary theme"
+    "DynamicFieldLabel": "Connect to a search box Web Part",
+    "NavNodeLabel": "Navigation nodes",
+    "NavNodeHeader": "Navigation nodes",
+    "NavNodeManageBtnLabel": "Edit navigation nodes",
+    "NavNodeDisplayTextFieldLabel": "Displaytext",
+    "NavNodeUrlFieldLabel": "Url",
+    "ColorPickerLabel": "Color",
+    "UseThemeColorLabel": "Use primary theme",
+    "PlaceHolderEditLabel": "Edit",
+    "PlaceHolderConfigureBtnLabel": "Configure",
+    "PlaceHolderIconText": "Search Navigation Web Part",
+    "PlaceHolderDescription": "This component displays search navigation",
+    "UseNlpValueLabel": "Use Natural Language Processing query value"
   }
 });
