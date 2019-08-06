61e547c6bb0fe2345e249e1ddbed2d9e66c6c2be
Albert-Jan Schot <appie@digiwijs.nl>
2019-08-06 11:10:36 +0200

    [chore] added Dutch localization (#247)

---
+++ solutions/ModernSearch/react-search-refiners/spfx/src/webparts/searchPagination/loc/nl-nl.js
@@ -0,0 +1,14 @@
+define([], function() {
+  return {
+    "PaginationConfigurationGroupName": "Paginering configuratie",
+    "SearchPaginationLabel": "Zoekpaginering",
+    "SearchResultsLabel": "Zoekresultaten",
+    "PlaceHolderEditLabel": "Bewerken",
+    "PlaceHolderConfigureBtnLabel": "Configureren",
+    "PlaceHolderIconText": "Zoekpaginering webonderdeel",
+    "PlaceHolderDescription": "Dit onderdeel toont zoekpaginering",
+    "CurrentPageLabel": "Huidige pagina",
+    "ConnectToSearchResultsLabel": "Verbind met een zoek webonderdeel",
+    "ShowBlankEditInfoMessage": "Geen pagina informatie om te tonen, het onderdeel blijft leeg in de weergave modus."
+  }
+});
