6a1c57d90023e49d8e5a139f47c4a171934cb0e7
Mikael Svenson <miksvenson@gmail.com>
2019-09-27 16:19:56 +0200

    Added build optimizations

--- solutions/ModernSearch/react-search-refiners/spfx/gulpfile.js
+++ solutions/ModernSearch/react-search-refiners/spfx/gulpfile.js
@@ -7,44 +7,71 @@ const bundleAnalyzer = require('webpack-bundle-analyzer');
 
 build.addSuppression(/^Warning - \[sass\].*$/);
 
-/********************************************************************************************
- * Adds an alias for handlebars in order to avoid errors while gulping the project
- * https://github.com/wycats/handlebars.js/issues/1174
- * Adds a loader and a node setting for webpacking the handlebars-helpers correctly
- * https://github.com/helpers/handlebars-helpers/issues/263
- ********************************************************************************************/
-build.configureWebpack.mergeConfig({
-  additionalConfiguration: (generatedConfiguration) => {
-
-    generatedConfiguration.resolve.alias = { handlebars: 'handlebars/dist/handlebars.min.js' };
-
-    generatedConfiguration.module.rules.push(
-      { 
-        test: /utils\.js$/, 
-        loader: 'unlazy-loader', 
-        include: [
-            /node_modules/,
-        ]  
-      }
-    );
-
-    generatedConfiguration.node = {
-      fs: 'empty'
+const envCheck = build.subTask('environmentCheck', (gulp, config, done) => {
+    if (!config.production) {
+        //https://spblog.net/post/2019/09/18/spfx-overclockers-or-how-to-significantly-improve-your-sharepoint-framework-build-performance#h_296972879501568737888136
+        build.tslintCmd.enabled = false;
+        //build.tscCmd.enabled = false;
     }
+    build.configureWebpack.mergeConfig({
+        additionalConfiguration: (generatedConfiguration) => {
 
-    const lastDirName = path.basename(__dirname);
-      const dropPath = path.join(__dirname, 'temp', 'stats');
-      generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: 'static',
-        reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
-        generateStatsFile: true,
-        statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
-        logLevel: 'error'
-      }));
-    
-    return generatedConfiguration;
-  }
+            /********************************************************************************************
+             * Adds an alias for handlebars in order to avoid errors while gulping the project
+             * https://github.com/wycats/handlebars.js/issues/1174
+             * Adds a loader and a node setting for webpacking the handlebars-helpers correctly
+             * https://github.com/helpers/handlebars-helpers/issues/263
+             ********************************************************************************************/
+            generatedConfiguration.resolve.alias = { handlebars: 'handlebars/dist/handlebars.min.js' };
+
+            generatedConfiguration.module.rules.push({
+                test: /utils\.js$/,
+                loader: 'unlazy-loader',
+                include: [
+                    /node_modules/,
+                ]
+            });
+
+            generatedConfiguration.node = {
+                fs: 'empty'
+            }
+
+            const lastDirName = path.basename(__dirname);
+            const dropPath = path.join(__dirname, 'temp', 'stats');
+            generatedConfiguration.plugins.push(new bundleAnalyzer.BundleAnalyzerPlugin({
+                openAnalyzer: false,
+                analyzerMode: 'static',
+                reportFilename: path.join(dropPath, `${lastDirName}.stats.html`),
+                generateStatsFile: true,
+                statsFilename: path.join(dropPath, `${lastDirName}.stats.json`),
+                logLevel: 'error'
+            }));
+
+            // Optimize build times - https://www.eliostruyf.com/speed-sharepoint-framework-builds-wsl-2/
+            if (!config.production) {
+                for (const rule of generatedConfiguration.module.rules) {
+                    // Add include rule for webpack's source map loader
+                    if (rule.use && typeof rule.use === 'string' && rule.use.indexOf('source-map-loader') !== -1) {
+                        rule.include = [
+                            path.resolve(__dirname, 'lib')
+                        ]
+                    }
+
+                    // Disable minification for css-loader
+                    if (rule.use && rule.use instanceof Array && rule.use.length == 2 && rule.use[1].loader && rule.use[1].loader.indexOf('css-loader') !== -1) {
+                        rule.use[1].options.minimize = false;
+                    }
+                }
+
+            }
+            return generatedConfiguration;
+        }
+    });
+
+    done();
 });
+build.rig.addPreBuildTask(envCheck);
+
+
 
 build.initialize(gulp);
