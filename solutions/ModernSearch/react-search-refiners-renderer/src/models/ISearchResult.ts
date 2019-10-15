7d0ce0ca4bc56dbae59c30681c8769b62e078426
Mikael Svenson <miksvenson@gmail.com>
2019-10-15 09:06:22 +0200

    Update ISearchResult.ts
    Fixed indenting for easier comparison

--- solutions/ModernSearch/react-search-refiners-renderer/src/models/ISearchResult.ts
+++ solutions/ModernSearch/react-search-refiners-renderer/src/models/ISearchResult.ts
@@ -1,59 +1,59 @@
 export interface ISearchResults {
-  QueryKeywords: string;
-  PaginationInformation?: IPaginationInformation;
-  RelevantResults: ISearchResult[];
-  RefinementResults: IRefinementResult[];
-  PromotedResults?: IPromotedResult[];
-  SecondaryResults?: ISearchResultBlock[];
+    QueryKeywords: string;
+    PaginationInformation?: IPaginationInformation;
+    RelevantResults: ISearchResult[];
+    RefinementResults: IRefinementResult[];
+    PromotedResults?: IPromotedResult[];
+    SecondaryResults?: ISearchResultBlock[];
 }
 
 export interface IPaginationInformation {
-  CurrentPage: number;
-  TotalRows: number;
-  MaxResultsPerPage: number;
+    CurrentPage: number;
+    TotalRows: number;
+    MaxResultsPerPage: number;
 }
 
 export interface ISearchVerticalInformation {
-  VerticalKey: string;
-  Count: number;
+    VerticalKey: string;
+    Count: number;
 }
 
 export interface ISearchResult {
-  [key: string]: string;
-  IconSrc?: string;
-  IconExt?: string;
+    [key: string]: string;
+    IconSrc?: string;
+    IconExt?: string;
 }
 
 export interface IRefinementResult {
-  FilterName: string;
-  Values: IRefinementValue[];
+    FilterName: string;
+    Values: IRefinementValue[];
 }
 
 export interface IPromotedResult {
-  Url: string;
-  Title: string;
-  Description: string;
+    Url: string;
+    Title: string;
+    Description: string;
 }
 
 export interface IRefinementValue {
-  RefinementCount: number;
-  RefinementName: string;
-  RefinementToken: string;
-  RefinementValue: string;
+    RefinementCount: number;
+    RefinementName: string;
+    RefinementToken: string;
+    RefinementValue: string;
 }
 
 export interface IRefinementFilter {
-  FilterName: string;
-  Values: IRefinementValue[];
-  Operator: RefinementOperator;
+    FilterName: string;
+    Values: IRefinementValue[];
+    Operator: RefinementOperator;
 }
 
 export enum RefinementOperator {
-  OR = 'or',
-  AND = 'and'
+    OR = 'or',
+    AND = 'and'
 }
 
 export interface ISearchResultBlock {
-  Title: string;
-  Results: ISearchResult[];
+    Title: string;
+    Results: ISearchResult[];
 }
