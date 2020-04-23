5cf6d4978031320b55c3fb4e5fd9b5d60d076e24
Waldek Mastykarz <waldek@mastykarz.nl>
2020-04-23 13:10:27 +0200

    Upgrades Inventory solution to SPFx v1.10

--- solutions/InventoryCheckOut/src/webparts/inventoryCheckOutWebPart/InventoryCheckOutWebPartWebPart.ts
+++ solutions/InventoryCheckOut/src/webparts/inventoryCheckOutWebPart/InventoryCheckOutWebPartWebPart.ts
@@ -4,12 +4,8 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Environment, EnvironmentType, Version } from '@microsoft/sp-core-library';
-import {
-  BaseClientSideWebPart,
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField,
-  PropertyPaneCheckbox
-} from '@microsoft/sp-webpart-base';
+import { BaseClientSideWebPart } from "@microsoft/sp-webpart-base";
+import { IPropertyPaneConfiguration, PropertyPaneCheckbox, PropertyPaneTextField } from "@microsoft/sp-property-pane";
 import { escape } from '@microsoft/sp-lodash-subset';
 import * as strings from 'inventoryCheckOutWebPartStrings';
 import { IInventoryCheckOutWebPartWebPartProps } from './IInventoryCheckOutWebPartWebPartProps';
